85876fdb9e30ccf201643bef636de6ce9ddae5d3
kempo <ilestkempo@gmail.com>
2018-03-31 18:35:23 -0700

    added ping/pong test

--- flask-test.py
+++ flask-test.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
-from datetime import datetime
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def homepage():
-    time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
     author1 = "Aaron Chen"
     author2 = "Tim Cheng"
-    return render_template("index.html", author1=author1, author2=author2, time=time)
+    return render_template("index.html", author1=author1, author2=author2)
+
+
+@app.route("/ping")
+def test():
+    return "pong"
 
 
 if __name__ == '__main__':
